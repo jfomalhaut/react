475ce6cb0055e6737949ee1d5f4a0f55a6ed3c79
hyeju.kwon <a01095021970@gmail.com>
2020-03-09 21:29:37 +0900

    massage

---
+++ 수강생/hyeju/start/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css'
+
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            count: 10
+        }
+    }
+
+    componentDidMount() {
+        console.log('component did mount')
+    }
+
+    componentDidUpdate(){
+        console.log('update!')
+    }
+
+    inscresement(){
+        this.setState({ count: this.state.count + 1});
+    }
+
+    decresement(){
+        this.setState({ count: this.state.count - 1});
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>App Component</h1>
+                <h2>{this.state.count}</h2>
+                <button onClick={this.incresement}>증가</button>
+                <button onClick={this.decresement}>감소</button>
+            </div>
+        );
+    }
+};
