fdc1f7632d9989c1cd7887571d1e3f23c365e378
hyejichoi <choihyeji2952@gmail.com>
2020-03-10 12:35:03 +0900

    pt2

---
+++ 수강생/choihyeji/setting4/src/component/Test2.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import '../App.css';
+
+const Test2 = () => {
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(2);
+
+    const incresement = () => {
+        setCount(count + 1);
+    };
+
+    const decresement = () => {
+        setCount(count - 1);
+    };
+
+    const multiple = () => {
+        setCount2(count2 * 2);
+    };
+
+    return (
+        <div>
+            <h1>I am Iron man</h1>
+            <h2>{count}</h2>
+            <h2>{count2}</h2>
+            <button onClick={incresement}>증가</button>
+            <button onClick={decresement}>감소</button>
+            <button onClick={multiple}>따블</button>
+        </div>
+    );
+};
+
+export default Test2;
---
+++ 수강생/choihyeji/setting4/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test2 from './component/Test2';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Test2/>, document.getElementById('root'));
