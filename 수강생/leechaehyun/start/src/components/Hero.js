25a5a70e060130018af40b9af402d2ec9c7fb9fa
LeeChaeHyun <dlcogus0317@gmail.com>
2020-03-10 16:19:02 +0900

    pppp

--- 수강생/leechaehyun/start/src/components/Hero.js
+++ 수강생/leechaehyun/start/src/components/Hero.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 
-function Hero() {
+const Hero = () => {
+    //useState(초기값)
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(100);
+    const [count3, setCount3] = useState(1);
+
+    //function increasement(){} 와 같은 방식
     const increasement = () => {
         setCount(count + 1);
-    };
-    
+    }
+
+    const decreasement = () => {
+        setCount(count2 - 1);
+    }
+
+    const multiple = () => {
+        setCount(count * 2);
+    }
+
     return (
         <div>
-            <h1>I'm Hero...</h1>
+            <h1>I'm Hero</h1>
             <h2>{count}</h2>
+            <h2>{count2}</h2>
+            <h2>{count3}</h2>
             <button onClick={increasement}>증가</button>
+            <button onClick={decreasement}>감소</button>
+            <button onClick={multiple}>곱하기</button>
         </div>
     );
 };
--- 수강생/leechaehyun/start/src/index.js
+++ 수강생/leechaehyun/start/src/index.js
@@ -1,10 +1,7 @@
-console.log('npm start');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hero from '../components/Hero';
+import Hero from './components/Hero';
 // import App from './App';
 // import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-
+ReactDOM.render(<Hero/>, document.getElementById('root'));
