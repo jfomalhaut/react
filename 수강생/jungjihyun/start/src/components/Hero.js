918bc54660272a4490086623cb87f0cd72821569
dlalsgh1120 <dlalsgh1120@gmail.com>
2020-03-10 17:19:46 +0900

    업데이트

--- 수강생/jungjihyun/start/src/components/Hero.js
+++ 수강생/jungjihyun/start/src/components/Hero.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 
 const Hero = () => {
     const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(100);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(10);
 
     const increasement = () => {
         setCount(count + 1);
     };
+    const decresement = () => {
+        setCount2(count2 - 1);
+    };
     const multiple = () => {
-        setCount2(count2 * 2);
-    }
+        setCount3(count3 * 2);
+    };
 
 
     return(
@@ -17,7 +21,9 @@ const Hero = () => {
             <h1>I'm Hero...</h1>
             <h2>{count}</h2>
             <h2>{count2}</h2>
+            <h2>{count3}</h2>
             <button onClick={increasement}>증가</button>
+            <button onClick={decresement}>감소</button>
             <button onClick={multiple}>곱하기</button>
         </div>
     );
