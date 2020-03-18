c2774b2dc049e73de38a4f45e94c3b8ec378d99f
bhsook123 <bhsook123@bitbucket.org>
2020-03-19 00:11:17 +0900

    dd

---
+++ 수강생/baek/hook/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Input from './routes/Input'
+
+ReactDOM.render(<Input/>, document.getElementById('root'));
