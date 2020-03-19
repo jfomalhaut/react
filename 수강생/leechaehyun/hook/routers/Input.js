f48d8c155366f71caf1232dba72b1a0258350dba
LeeChaeHyun <dlcogus0317@gmail.com>
2020-03-19 15:11:53 +0900

    sss

--- 수강생/leechaehyun/hook/routers/Input.js
+++ 수강생/leechaehyun/hook/routers/Input.js
@@ -1,55 +1,46 @@
-import React, { useState } from 'react';
-
-const getAverage = (numbers) => {
-    if (numbers.length === 0) {
-        return 0;
-    }
-    const sum = numbers.reduce((acc, cur) => acc + cur);
-    const avg = sum / numbers.length;
-    return Math.round(avg);
-};
+import React, { useState, useRef } from 'react';
+import getAverage from '../pipes/Average';
 
 const Input = () => {
-    const [number, setNumber] = useState(0);
-    const [list, setList] = useState([]);
-    const inputElement = useRef();
+	const [number, setNumber] = useState('');
+	const [list, setList] = useState([]);
+	const inputElement = useRef();
 
-    const handler = event => {
-        const { target: { value } } = event;
-        // console.log(event.target.value)
-        setNumber(parseInt(value));
-    };
+	const handler = event => {
+		const { target: { value } } = event;
+		setNumber(parseInt(value));
+	};
 
-const onInsert = () => {
-    const nextList = list.concat(number);
-    setList(nextList);
-    setNumber('');
-    inputElement.current.focus();
-};
+	const onInsert = () => {
+		const nextList = list.concat(number);
+		setList(nextList);
+		setNumber('');
+		inputElement.current.focus();
+	};
 
-const onEnter = event => {
-    if (event.key === 'Enter') {
-        onInsert();
-    }
-};
+	const onEnter = event => {
+		if (event.key === 'Enter') {
+			onInsert();
+		}
+	};
 
-    return (
-        <div>
-            <input ref={inputElement} value={number} onChange={handler} onKeyPress={onEnter} placeholder="입력하세요"/>
-            <button onClick={onInsert}>입력</button>
-            <ul>
-                {list.map((item, idx) => (
-                    <li key={idx}>{item}</li>
-                ))}
-            </ul>
-            <div>
-                현재 입력된 값 : { number } 
-            </div>
-            <div>
-                전체의 평균 값 : { getAverage(list) } 
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<input ref={inputElement} value={number} onChange={handler} onKeyPress={onEnter} placeholder="입력하세요"/>
+			<button onClick={onInsert}>입력</button>
+			<ul>
+				{list.map((item, idx) => (
+					<li key={idx}>{item}</li>
+				))}
+			</ul>
+			<div>
+				현재 입력된 값 : { number }
+			</div>
+			<div>
+				전체의 평균 값 : { getAverage(list) }
+			</div>
+		</div>
+	);
 };
 
 export default Input;
--- 수강생/leechaehyun/hook/src/App.js
+++ 수강생/leechaehyun/hook/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+	const [data, setData] = useState(0);
+	const [category, setCategory] = useState(0);
+
+	const start = () => {
+		setData(data + 1);
+	};
+
+	useEffect(() => {
+		console.log('start');
+	}, []);
+
+	useEffect(() => {
+		console.log('data값이 변경 되었습니다' + data);
+	}, [data]);
+
+	useEffect(() => {
+		switch(category) {
+			case 0 : console.log('전체보기'); break;
+			case 1 : console.log('카테고리1'); break;
+			case 2 : console.log('카테고리2'); break;
+			case 3 : console.log('카테고리3'); break;
+		}
+	}, [category]);
+
 	return (
-        <h1>hi</h1>
-    );
+		<div>
+			<h1>{data}</h1>
+			<button onClick={start}>btn</button>
+			<button onClick={() => setCategory(0)}>전체보기</button>
+			<button onClick={() => setCategory(1)}>흐물거리는</button>
+			<button onClick={() => setCategory(2)}>등푸른</button>
+			<button onClick={() => setCategory(3)}>갑각류</button>
+		</div>
+	);
 };
 
 export default App;
