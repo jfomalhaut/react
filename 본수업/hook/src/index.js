14237b235e2f57595d0d9c37caa12d89adfa1b76
이준 <dlwnssla@jfomalhaut.com>
2020-03-19 22:04:18 +0900

    200319 수업

--- 본수업/hook/src/index.js
+++ 본수업/hook/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';;
 import ReactDOM from 'react-dom';
 import App from './App';
 import Input from './routers/Input';
+import Input2 from './routers/Input2';
 
-ReactDOM.render(<Input/>, document.getElementById('root'));
+ReactDOM.render(<Input2/>, document.getElementById('root'));
